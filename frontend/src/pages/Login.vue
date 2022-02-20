f862f97376618206c000cfd37634c4b3062bc1ad
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2022-02-20 08:29:17 +0500

    :zap: Error handling for login and register

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -3,9 +3,24 @@
     <form @submit.prevent="login">
       <label for="username">Username</label>
       <input v-model="username" type="text" name="username" value />
+      <ul>
+        <li v-for="(error, index) in usernameErrors" :key="index">
+          {{ error }}
+        </li>
+      </ul>
       <label for="password">Password</label>
       <input v-model="password" type="text" name="password" value />
+      <ul>
+        <li v-for="(error, index) in passwordErrors" :key="index">
+          {{ error }}
+        </li>
+      </ul>
       <button type="submit" name="button">Login</button>
+      <ul>
+        <li v-for="(error, index) in nonFieldErrors" :key="index">
+          {{ error }}
+        </li>
+      </ul>
     </form>
   </div>
 </template>
@@ -18,6 +33,9 @@ export default defineComponent({
     return {
       username: "",
       password: "",
+      usernameErrors: [],
+      passwordErrors: [],
+      nonFieldErrors: [],
     };
   },
   methods: {
@@ -29,6 +47,11 @@ export default defineComponent({
         })
         .then(() => {
           this.$router.push({ name: "dashboard" });
+        })
+        .catch((err) => {
+          this.usernameErrors = err.response.data.username;
+          this.passwordErrors = err.response.data.password;
+          this.nonFieldErrors = err.response.data.non_field_errors;
         });
     },
   },
--- frontend/src/pages/Register.vue
+++ frontend/src/pages/Register.vue
@@ -3,11 +3,31 @@
     <form @submit.prevent="register">
       <label for="username">Username</label>
       <input v-model="username" type="text" name="username" value />
+      <ul>
+        <li v-for="(error, index) in usernameErrors" :key="index">
+          {{ error }}
+        </li>
+      </ul>
       <label for="password1">Password</label>
       <input v-model="password1" type="text" name="password1" value />
+      <ul>
+        <li v-for="(error, index) in password1Errors" :key="index">
+          {{ error }}
+        </li>
+      </ul>
       <label for="password2">Password Again</label>
       <input v-model="password2" type="text" name="password2" value />
+      <ul>
+        <li v-for="(error, index) in password2Errors" :key="index">
+          {{ error }}
+        </li>
+      </ul>
       <button type="submit" name="button">Register</button>
+      <ul>
+        <li v-for="(error, index) in nonFieldErrors" :key="index">
+          {{ error }}
+        </li>
+      </ul>
     </form>
   </div>
 </template>
@@ -21,6 +41,10 @@ export default defineComponent({
       username: "",
       password1: "",
       password2: "",
+      usernameErrors: [],
+      password1Errors: [],
+      password2Errors: [],
+      nonFieldErrors: [],
     };
   },
   methods: {
@@ -33,6 +57,12 @@ export default defineComponent({
         })
         .then(() => {
           this.$router.push({ name: "dashboard" });
+        })
+        .catch((err) => {
+          this.usernameErrors = err.response.data.username;
+          this.password1Errors = err.response.data.password1;
+          this.password2Errors = err.response.data.password2;
+          this.nonFieldErrors = err.response.data.non_field_errors;
         });
     },
   },
