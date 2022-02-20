15e11df6745a1772e71da1b8c426b9d4ffc07b83
Sarmad Gulzar <sarmadgulzar@hotmail.com>
2022-02-20 13:01:38 +0500

    :bug: Designed the register/login forms

--- frontend/src/pages/Login.vue
+++ frontend/src/pages/Login.vue
@@ -1,59 +1,107 @@
 <template>
-  <div>
-    <form @submit.prevent="login">
-      <label for="username">Username</label>
-      <input v-model="username" type="text" name="username" value />
-      <ul>
-        <li v-for="(error, index) in usernameErrors" :key="index">
-          {{ error }}
-        </li>
-      </ul>
-      <label for="password">Password</label>
-      <input v-model="password" type="text" name="password" value />
-      <ul>
-        <li v-for="(error, index) in passwordErrors" :key="index">
-          {{ error }}
-        </li>
-      </ul>
-      <button type="submit" name="button">Login</button>
-      <ul>
-        <li v-for="(error, index) in nonFieldErrors" :key="index">
-          {{ error }}
-        </li>
-      </ul>
+  <div class="flex justify-center q-mt-xl">
+    <form @submit.prevent="login" style="width: 300px">
+      <q-input
+        class="q-mb-sm"
+        filled
+        bottom-slots
+        v-model="username"
+        label="Username"
+        :no-error-icon="true"
+        :error-message="usernameError"
+        :error="!!usernameError"
+      />
+      <q-input
+        class="q-mb-sm"
+        filled
+        bottom-slots
+        v-model="password"
+        label="Password"
+        type="password"
+        :no-error-icon="true"
+        :error-message="passwordError"
+        :error="!!passwordError"
+      />
+      <q-btn
+        class="q-mt-sm"
+        :loading="buttonLoading"
+        type="submit"
+        color="primary"
+        name="button"
+        label="Login"
+      />
     </form>
   </div>
 </template>
 <script>
-import { defineComponent } from "vue";
+import { useQuasar } from "quasar";
+import { defineComponent, ref } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
   name: "Login",
-  data() {
+  setup() {
+    const $q = useQuasar();
+    const $store = useStore();
+    const $router = useRouter();
+
+    const username = ref("");
+    const usernameError = ref("");
+
+    const password = ref("");
+    const passwordError = ref("");
+
+    const buttonLoading = ref(false);
+
     return {
-      username: "",
-      password: "",
-      usernameErrors: [],
-      passwordErrors: [],
-      nonFieldErrors: [],
+      username,
+      usernameError,
+      password,
+      passwordError,
+      buttonLoading,
+      login() {
+        buttonLoading.value = true;
+        $store
+          .dispatch("auth/login", {
+            username: username.value,
+            password: password.value,
+          })
+          .then(() => {
+            buttonLoading.value = false;
+            $q.notify({
+              icon: "done",
+              color: "positive",
+              message: "Logged in",
+              timeout: 500,
+            });
+            setTimeout(() => {
+              $router.push({ name: "dashboard" });
+            }, 1000);
+          })
+          .catch((err) => {
+            let get_first_or_null = (arr) => {
+              if (!arr) {
+                return null;
+              } else {
+                return arr[0];
+              }
+            };
+            buttonLoading.value = false;
+            let data = err.response.data;
+            usernameError.value = get_first_or_null(data.username);
+            passwordError.value = get_first_or_null(data.password);
+            let non_field_error = get_first_or_null(data.non_field_errors);
+
+            if (non_field_error) {
+              $q.notify({
+                color: "negative",
+                message: non_field_error,
+              });
+            }
+          });
+      },
     };
-  },
-  methods: {
-    login() {
-      this.$store
-        .dispatch("auth/login", {
-          username: this.username,
-          password: this.password,
-        })
-        .then(() => {
-          this.$router.push({ name: "dashboard" });
-        })
-        .catch((err) => {
-          this.usernameErrors = err.response.data.username;
-          this.passwordErrors = err.response.data.password;
-          this.nonFieldErrors = err.response.data.non_field_errors;
-        });
-    },
   },
 });
 </script>
--- frontend/src/pages/Register.vue
+++ frontend/src/pages/Register.vue
@@ -1,70 +1,125 @@
 <template>
-  <div>
-    <form @submit.prevent="register">
-      <label for="username">Username</label>
-      <input v-model="username" type="text" name="username" value />
-      <ul>
-        <li v-for="(error, index) in usernameErrors" :key="index">
-          {{ error }}
-        </li>
-      </ul>
-      <label for="password1">Password</label>
-      <input v-model="password1" type="text" name="password1" value />
-      <ul>
-        <li v-for="(error, index) in password1Errors" :key="index">
-          {{ error }}
-        </li>
-      </ul>
-      <label for="password2">Password Again</label>
-      <input v-model="password2" type="text" name="password2" value />
-      <ul>
-        <li v-for="(error, index) in password2Errors" :key="index">
-          {{ error }}
-        </li>
-      </ul>
-      <button type="submit" name="button">Register</button>
-      <ul>
-        <li v-for="(error, index) in nonFieldErrors" :key="index">
-          {{ error }}
-        </li>
-      </ul>
+  <div class="flex justify-center q-mt-xl">
+    <form @submit.prevent="register" style="width: 300px">
+      <q-input
+        class="q-mb-sm"
+        filled
+        bottom-slots
+        v-model="username"
+        label="Username"
+        :no-error-icon="true"
+        :error-message="usernameError"
+        :error="!!usernameError"
+      />
+      <q-input
+        class="q-mb-sm"
+        filled
+        bottom-slots
+        v-model="password1"
+        label="Password"
+        type="password"
+        :no-error-icon="true"
+        :error-message="password1Error"
+        :error="!!password1Error"
+      />
+      <q-input
+        class="q-mb-sm"
+        filled
+        bottom-slots
+        v-model="password2"
+        label="Password Again"
+        type="password"
+        :no-error-icon="true"
+        :error-message="password2Error"
+        :error="!!password2Error"
+      />
+      <q-btn
+        class="q-mt-sm"
+        :loading="buttonLoading"
+        type="submit"
+        color="primary"
+        name="button"
+        label="Register"
+      />
     </form>
   </div>
 </template>
 <script>
-import { defineComponent } from "vue";
+import { useQuasar } from "quasar";
+import { defineComponent, ref } from "vue";
+import { useStore } from "vuex";
+import { useRouter } from "vue-router";
 
 export default defineComponent({
   name: "Register",
-  data() {
+  setup() {
+    const $q = useQuasar();
+    const $store = useStore();
+    const $router = useRouter();
+
+    const username = ref("");
+    const usernameError = ref("");
+
+    const password1 = ref("");
+    const password1Error = ref("");
+
+    const password2 = ref("");
+    const password2Error = ref("");
+
+    const buttonLoading = ref(false);
+
     return {
-      username: "",
-      password1: "",
-      password2: "",
-      usernameErrors: [],
-      password1Errors: [],
-      password2Errors: [],
-      nonFieldErrors: [],
+      username,
+      usernameError,
+      password1,
+      password1Error,
+      password2,
+      password2Error,
+      buttonLoading,
+      register() {
+        buttonLoading.value = true;
+        $store
+          .dispatch("auth/register", {
+            username: username.value,
+            password1: password1.value,
+            password2: password2.value,
+          })
+          .then(() => {
+            buttonLoading.value = false;
+            $q.notify({
+              icon: "done",
+              color: "positive",
+              message: "Logged in",
+              timeout: 500,
+            });
+            setTimeout(() => {
+              $router.push({ name: "dashboard" });
+            }, 1000);
+          })
+          .catch((err) => {
+            let get_first_or_null = (arr) => {
+              if (!arr) {
+                return null;
+              } else {
+                return arr[0];
+              }
+            };
+            buttonLoading.value = false;
+            let data = err.response.data;
+            usernameError.value = get_first_or_null(data.username);
+            password1Error.value = get_first_or_null(data.password1);
+            password2Error.value = get_first_or_null(data.password2);
+            let non_field_error = get_first_or_null(data.non_field_errors);
+
+            if (non_field_error) {
+              $q.notify({
+                color: "negative",
+                message: non_field_error,
+              });
+            }
+          });
+      },
     };
-  },
-  methods: {
-    register() {
-      this.$store
-        .dispatch("auth/register", {
-          username: this.username,
-          password1: this.password1,
-          password2: this.password2,
-        })
-        .then(() => {
-          this.$router.push({ name: "dashboard" });
-        })
-        .catch((err) => {
-          this.usernameErrors = err.response.data.username;
-          this.password1Errors = err.response.data.password1;
-          this.password2Errors = err.response.data.password2;
-          this.nonFieldErrors = err.response.data.non_field_errors;
-        });
-    },
   },
 });
 </script>
